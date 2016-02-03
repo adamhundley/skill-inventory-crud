6bfb51f6fdc8bf38b105e0d3a099a7631d2821ac
Adam Hundley <adamhundley@gmail.com>
2016-02-03 08:41:05 -0700

    first commit, no styling

--- app/models/skill_inventory.rb
+++ app/models/skill_inventory.rb
@@ -1,5 +1,53 @@
+require 'yaml/store'
+require 'models/skill'
+
 class SkillInventory
-  def self.database
-    @database ||= YAML::Store.new("db/skill_inventory")
+  attr_reader :database
+
+  def initialize(database)
+    @database = database
+  end
+
+  def create(skill)
+    database.transaction do
+      database["skills"] ||= []
+      database["next_id"] ||= 0
+      database["next_id"] += 1
+      database["skills"] << {"id" => database["next_id"],
+                            "name" => skill["name"],
+                            "status" => skill["status"]}
+    end
+  end
+
+  def raw_skills
+    database.transaction do
+      database["skills"] || []
+    end
+  end
+
+  def raw_skill(id)
+    raw_skills.find { |skill| skill["id"] == id }
+  end
+
+  def find(id)
+    Skill.new(raw_skill(id))
+  end
+
+  def all
+    raw_skills.map { |skill| Skill.new(skill)}
+  end
+
+  def delete(id)
+    database.transaction do
+      database["skills"].delete_if { |skill| skill["id"] == id }
+    end
+  end
+
+  def update(task, id)
+    database.transaction do
+      target_skill = database["skills"].find { |s| s["id"] == id }
+      target_skill["name"] = skill[:name]
+      target_skill["status"] = skill[:status]
+    end
   end
 end
